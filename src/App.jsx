002835d05ea3ba1881c13ee76cf2d3c61c917753
Sophia Leme <sophia.lemet1@gmail.com>
2025-04-04 12:02:47 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Banner from './components/Banner'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header/>
-      <div id="baner"></div>
+      <Banner/>
       <div className="re">
         <div className="re1">
             <p>Agende Sua</p>
