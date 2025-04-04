837dc52325cc92b93a1e0c80e933015faf2f3e6a
Sophia Leme <sophia.lemet1@gmail.com>
2025-04-04 12:11:32 -0300

    commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './ProductList.css'
+
+function ProductList(){
+    return (
+        <>
+           <div className="re">
+             <div className="re1">
+                 <p>Agende Sua</p>
+                 <p>Sessão</p>
+             </div>
+             <div className="re2">
+                 <p>Veja Nossas</p>
+                 <p>Promoções</p>
+             </div>
+           </div>
+           <div className="retangulo">
+             <div className="retangulo1"> <p>Pacote</p><p>Básico</p><p>R$ 400,00</p> </div>
+             <div className="retangulo2"> <p>Pacote</p><p>Família</p><p>R$ 1.000,00</p> </div>
+             <div className="retangulo3"> <p>Pacote</p><p>Premium</p><p>R$ 2.000,00</p> </div>
+             <div className="retangulo4"> <p>Pacote</p><p>Casamento</p><p>R$ 6.000,00</p> </div>
+             <div className="retangulo5"> <p>Pacote</p><p>Empresarial</p><p>R$ 1.500,00</p> </div>
+             <div className="retangulo6"> <p>Pacote</p><p>Infantil</p><p>R$ 800,00</p> </div>
+             <div className="retangulo7"> <p>Pacote</p><p>Temático</p><p>R$ 1.200,00</p> </div>
+             <div className="retangulo8"> <p>Pacote de</p><p>Assinatura</p><p>R$ 1.500,00</p><p> por mês</p> </div>
+             <div className="retangulo9"> <p>Pacote Mini</p><p>Ensaio</p><p>R$ 300,00</p> </div>
+             <div className="retangulo0"> <p>Pacote</p><p>Pets</p><p>R$ 700,00</p> </div>
+             <div className="retangulol"> <p>Pacote Evento</p><p>Social</p><p>R$ 2.000,00</p> </div>
+             <div className="retangulos"> <p>Pacote</p><p>Maternidade</p><p>R$ 3.500,00</p> </div>
+           </div>
+        </>
+    )
+}
+export default ProductList
